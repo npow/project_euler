7186f24cb7048403265790ad01b0c29bc65bd64c
Nissan Pow <nissan.pow@gmail.com>
2012-11-22 14:26:03 -0500

    .

--- 24/main.cc
+++ 24/main.cc
@@ -1,31 +1,14 @@
 #include <iostream>
-#include <vector>
+#include <algorithm>
 using namespace std;
 
-vector<vector<int> > permute(const vector<int>& digits, const vector<int>& curr) {
-  vector<vector<int> > res;
-  if (digits.empty()) {
-    res.push_back(curr);
-    return res;
-  }
-  for (int i = 0; i < digits.size(); ++i) {
-    vector<int> tmpCurr = curr; // copy
-    vector<int> tmpDigits = digits;
-    tmpDigits.erase(remove(tmpDigits.begin(), tmpDigits.end(), digits[i]), tmpDigits.end());
-    tmpCurr.push_back(digits[i]);
-    vector<vector<int> > v = permute(tmpDigits, tmpCurr);
-    res.insert(res.end(), v.begin(), v.end());
-  }
-  return res;
-}
-
 int main() {
-  vector<int> A;
-  for (int i = 0; i < 10; ++i) A.push_back(i);
-  vector<vector<int> > v = permute(A, vector<int>());
-  sort(v.begin(), v.end());
-
-  const vector<int>& p = v[1000000-1];
-  for (int i = 0; i < p.size(); ++i) cout << p[i] << " ";
+  int digits[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+  for (int i = 0; i < 1000000-1; ++i) {
+    next_permutation(digits, digits + 10);
+  }
+  for (int i = 0; i < 10; ++i) {
+    cout << digits[i];
+  }
   cout << endl;
 }
